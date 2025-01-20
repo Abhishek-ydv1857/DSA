0c9902ac18f7656e094366ad3e0e544062acbd2b
Abhishek <ayabhishek2003yadav@gmail.com>
2025-01-20 12:44:33 +0530

    print hello through function call

---
+++ function/A1.java
@@ -0,0 +1,10 @@
+public class A1{
+    public static void HelloPrint(){
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        HelloPrint();
+    }
+}
